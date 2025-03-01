e4c85108990db51eecda58ebdb038430345a86a4
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2025-03-01 08:40:47 +0100

    use lz4

--- demos/high_res/assets/img2c.py
+++ demos/high_res/assets/img2c.py
@@ -6,7 +6,7 @@ files = ""
 
 for i in sys.argv[1:]:
 
-    c = f"../../../scripts/LVGLImage.py  {i} --cf ARGB8888 --ofmt C"
+    c = f"../../../scripts/LVGLImage.py  {i} --cf ARGB8888 --ofmt C --compress LZ4"
     print(c)
     os.system(c)
 
