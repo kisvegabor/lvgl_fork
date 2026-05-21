04f71cc846308447778f736c4a8ef17cbc8496f9
Gabor Kiss-Vamosi <kisvegabor@gmail.com>
2026-05-21 13:17:20 +0200

    update  the script
    api map + minor fixes
    
    add names and remove flex add title
    
    minot fixes based on feedback

--- examples/get_started/get_started_slider/lv_example_get_started_slider.c
+++ examples/get_started/get_started_slider/lv_example_get_started_slider.c
@@ -28,16 +28,17 @@ void lv_example_get_started_slider(void)
     }
 
     lv_obj_t * screen = lv_screen_active();
-    lv_obj_set_flex_flow(screen, LV_FLEX_FLOW_COLUMN);
-    lv_obj_set_style_flex_cross_place(screen, LV_FLEX_ALIGN_CENTER, 0);
-    lv_obj_set_style_pad_row(screen, 16, 0);
 
     /* 💡 Drag the slider; the label tracks it because both share `subject_value`. */
-    lv_obj_t * lv_label_0 = lv_label_create(screen);
-    lv_label_bind_text(lv_label_0, &subject_value, "%d");
+    lv_obj_t * label = lv_label_create(screen);
+    lv_obj_set_align(label, LV_ALIGN_CENTER);
+    lv_obj_set_y(label, -20);
+    lv_label_bind_text(label, &subject_value, "%d");
 
-    lv_obj_t * lv_slider_0 = lv_slider_create(screen);
-    lv_obj_set_width(lv_slider_0, lv_pct(90));
-    lv_slider_bind_value(lv_slider_0, &subject_value);
+    lv_obj_t * slider = lv_slider_create(screen);
+    lv_obj_set_align(slider, LV_ALIGN_CENTER);
+    lv_obj_set_y(slider, 20);
+    lv_obj_set_width(slider, lv_pct(90));
+    lv_slider_bind_value(slider, &subject_value);
 }
 #endif
